1e74498b3e215c6a671e16db604d9c6eae72d8c7
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-06 17:27:10 +0000

    Queue v1.2 (Re-Upload)

---
+++ Queue Implementation/DJDQueue.cs
@@ -0,0 +1,59 @@
+namespace Queue_Implementation
+{
+    public class DJDQueue
+    {
+        private int[] queue;
+        private int index = 0;
+
+        //Construtor 1
+        public DJDQueue()
+        {
+            queue = new int[6];
+        }
+
+        //Construtor 2
+        public DJDQueue(int size)
+        {
+            queue = new int[size];
+        }
+
+        //Método Enqueue()
+        public void Enqueue(int n)
+        {
+            if (index < queue.Length)
+            {
+                queue[index++] = n;
+            }
+        }
+
+        //Método Dequeue()
+        public int Dequeue()
+        {
+            int first = queue[0]; //Indica o valor da primeira posição da Queue
+            if (index >= 0)
+            {
+                //Mover os outros valores de forma a que o "Dequeue()" retire o primeiro valor
+                for (int i = 0; i < queue.Length - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+
+                --index;
+                return first;
+            }
+
+            return 0;
+        }
+
+        //Método Peek()
+        public int Peek()
+        {
+            if (index >= 0)
+            {
+                return queue[--index];
+            }
+
+            return 0;
+        }
+    }
+}
