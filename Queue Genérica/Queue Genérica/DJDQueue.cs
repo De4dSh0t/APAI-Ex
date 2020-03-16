30d6bdcebaf82b16076c629fbf6865169d9e2667
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-16 17:57:22 +0000

    Queue Genérica V0.1

---
+++ Queue Genérica/Queue Genérica/DJDQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Queue_Genérica
+{
+    public class DJDQueue<T>
+    {
+        private T[] queue;
+
+        public DJDQueue()
+        {
+            queue = new T[6];
+        }
+
+        public DJDQueue(int s)
+        {
+            queue = new T[s];
+        }
+
+        public void Enqueue(int n)
+        {
+            
+        }
+    }
+}
