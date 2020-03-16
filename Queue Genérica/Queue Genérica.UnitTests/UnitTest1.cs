06a0e111a3972a0e97d0c82dbb879c8aaea5b538
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-16 18:58:36 +0000

    Added "Queue Genérica.UnitTests"

---
+++ Queue Genérica/Queue Genérica.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using Queue_Genérica;
+
+namespace Queue_Genérica.UnitTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
