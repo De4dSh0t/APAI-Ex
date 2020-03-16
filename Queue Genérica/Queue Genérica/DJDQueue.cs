3e0f426567ad1b0860d52c3149045e06f1815778
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-16 18:44:24 +0000

    Queue Genérica V0.2

--- Queue Genérica/Queue Genérica/DJDQueue.cs
+++ Queue Genérica/Queue Genérica/DJDQueue.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Queue_Genérica
 {
     public class DJDQueue<T>
     {
         private T[] queue;
+        private int addI = -1; //Index para Adicionar (Enqueue)
+        private int removeI = 0; //Index para Remover (Dequeue)
 
         public DJDQueue()
         {
@@ -16,9 +20,48 @@ namespace Queue_Genérica
             queue = new T[s];
         }
 
-        public void Enqueue(int n)
+        public void Enqueue(T n)
+        {
+            if ((addI == queue.Length-1 && removeI == 0) || addI == removeI)
+            {
+                throw new InvalidOperationException("Queue is full!");
+            }
+            else if (addI <= queue.Length-1)
+            {
+                queue[++addI] = n;
+            }
+            else if()
+            {
+                
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (removeI <= queue.Length - 1)
+            { 
+                return queue[removeI++];
+            }
+            else
+            {
+                removeI = 0;
+                return queue[removeI++];
+            }
+        }
+
+        public T Peek()
+        {
+            return queue[removeI];
+        }
+
+        public int Length()
         {
             
+        }
+
+        public int Capacity()
+        {
+            return queue.Length;
         }
     }
 }
