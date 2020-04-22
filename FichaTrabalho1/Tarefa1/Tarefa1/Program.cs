90ec88988e6eb05a6080bfd6f382ae95f3c79c03
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-22 11:50:53 +0100

    T1 v1.0 (Added "ReadText" class and updated "Program")

---
+++ FichaTrabalho1/Tarefa1/Tarefa1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarefa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadText rT = new ReadText("lyrics.txt");
+            string[] lyrics = rT.Read().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            for (int i = 0; i < lyrics.Length; i++)
+            {
+                if (wordCount.ContainsKey(lyrics[i])) //Verifica se uma das Keys tem um nome igual ao que vai ser inserido "lyrics[i]"
+                {
+                    wordCount[lyrics[i]] += 1;
+                }
+                else //Caso não tenha um nome igual, então adiciona a "lyrics[i]" como Key no dicionário
+                {
+                    wordCount.Add(lyrics[i], 1);
+                }
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("      Word Count      ");
+            Console.WriteLine("----------------------");
+            foreach (KeyValuePair<string, int> wC in wordCount)
+            {
+                Console.WriteLine($"{wC.Key}: {wC.Value}");
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine("    Repeated Words    ");
+            Console.WriteLine("----------------------");
+            foreach (var wC in wordCount)
+            {
+                if (wC.Value > 1)
+                {
+                    Console.WriteLine($"{wC.Key} as repeated {wC.Value} times.");
+                }
+            }
+        }
+    }
+}
---
+++ FichaTrabalho1/Tarefa1/Tarefa1/ReadText.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Tarefa1
+{
+    public class ReadText
+    {
+        private string fileName;
+
+        public ReadText(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public string Read()
+        {
+            return File.ReadAllText(fileName, Encoding.UTF8);
+        }
+    }
+}
