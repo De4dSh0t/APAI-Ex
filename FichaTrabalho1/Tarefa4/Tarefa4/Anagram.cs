a0c1edf3e9c01568db00f3a7c27d7b98a4b16680
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-22 16:23:16 +0100

    T4 v1.0 (Added class "Anagram" and updated "Program")

---
+++ FichaTrabalho1/Tarefa4/Tarefa4/Anagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tarefa4
+{
+    public class Anagram
+    {
+        public static bool IsAnagram(string s1, string s2)
+        {
+            //Se existir alguma diferença entre o tamanho das strings, então não é um anagrama
+            if (s1.Length < s2.Length)
+            {
+                return false;
+            }
+            
+            char[] c1 = s1.ToLower().ToCharArray();
+            char[] c2 = s2.ToLower().ToCharArray();
+            
+            //Ordena alfabeticamente o array de char
+            Array.Sort(c1);
+            Array.Sort(c2);
+            
+            //Como uma string é um array de chars, "s1" e "s2" tornam-se uma nova string dos respetivos arrays de char
+            s1 = new string(c1);
+            s2 = new string(c2);
+
+            return s1 == s2;
+        }
+    }
+}
---
+++ FichaTrabalho1/Tarefa4/Tarefa4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tarefa4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First word: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine("Second word: ");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine($"Is anagram? {Anagram.IsAnagram(word1, word2)}");
+        }
+    }
+}
